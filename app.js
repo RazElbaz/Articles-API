414edec5e22545f465748cef5a8bdc2dcd1341ed
unknown <raz.e@sightbit.com>
2024-07-01 09:39:28 +0300

    add Morgan and  middleware functions

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next(); // next( ) function is a callback that, when invoked, passes control to the next middleware function.
+});
+
 
 app.get('/' , (req, res) => {
     res.status(200).json({
         message: 'Hello World 3'
+    })
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     })
 });
 
